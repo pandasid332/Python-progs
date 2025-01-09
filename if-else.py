c45203916f99a96c93d7b2ee38cc8d2aed11ad0f
Sidharth Panda <sidharth.panda@hotmail.com>
2025-01-09 17:06:34 +0530

    if else program

---
+++ if-else.py
@@ -0,0 +1,23 @@
+a=int(input('Enter any digits: '))
+if a==1:
+    print('ONE')
+elif a==2:
+    print('TWO')
+elif a==3:
+    print('THREE')
+elif a==4:
+    print('FOUR')
+elif a==5:
+    print('FIVE')
+elif a==6:
+    print('SIX')
+elif a==7:
+    print('SEVEN')
+elif a==8:
+    print('EIGHT')
+elif a==9:
+    print('NINE')
+elif a==0:
+    print('ZERO')
+else:
+    print('This is incorrect')
