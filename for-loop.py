8c792ac15ea297ac7489b843ca1d35756002f3e3
Sidharth Panda <sidharth.panda@hotmail.com>
2025-01-10 16:09:45 +0530

    add fix

--- for-loop.py
+++ for-loop.py
@@ -2,7 +2,9 @@ lst=[10,20,30]
 tpl=(11,12,13)
 dct={1:'India',2:'Srilanka',3:'Bhutan'}
 
-
+for i in lst:
+    print('{}'.format(i),end=' ')
+print()
 for i in lst:
     print('Value in list: {}'.format(i))
 
