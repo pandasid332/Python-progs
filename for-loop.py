7e643453aca743de304e26fe575279ec74af3b0d
Sidharth Panda <sidharth.panda@hotmail.com>
2025-01-10 15:53:43 +0530

    For loop

---
+++ for-loop.py
@@ -0,0 +1,19 @@
+lst=[10,20,30]
+tpl=(11,12,13)
+dct={1:'India',2:'Srilanka',3:'Bhutan'}
+
+
+for i in lst:
+    print('Value in list: {}'.format(i))
+
+for i in tpl:
+    print('Value in tupple: {}'.format(i))
+
+for i in dct.keys():
+    print('Keys are: {}'.format(i))
+
+for i in dct.values():
+    print('Values are: {}'.format(i))
+
+for k,v in dct.items():
+    print('Key is: {} and value is: {}'.format(k,v))
