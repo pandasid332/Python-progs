a6a36ab139430a08d21a6436a4ff5752499f323d
Sidharth Panda <sidharth.panda@hotmail.com>
2025-01-12 08:46:43 +0530

    Turnery operator

--- lambda.py
+++ lambda.py
@@ -1,5 +1,5 @@
 add=lambda x=int(input('Enter first value: ')),y=int(input('Enter second value: ')) : x+y
-big=lambda x=float(input('Enter first name: ')),y=float(input('Enter second value')) : x if x>y else y
+big=lambda x=float(input('Enter first name: ')),y=float(input('Enter second value' )) : x if x>y else y  # Turnery operator
 
 x=add()
 print(x)
